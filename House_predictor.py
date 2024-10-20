2f54429dd3d01f4cc8e5225ded3a9f92e8925d51
Abhinandan Saini <123210753+AbhinandanS4@users.noreply.github.com>
2024-10-20 16:43:50 +0530

    Add files via upload

---
+++ House_predictor.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+df=pd.read_csv("E:\datasets\Housing.csv")
+
+df.drop(columns=['guestroom','basement','hotwaterheating','prefarea'],inplace=True)
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+le=LabelEncoder()
+Scr=StandardScaler()
+
+X=df.drop(columns=['price'])
+y=df['price']
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=12)
+
+def load_model():
+
+    with open('lr_model.pkl','rb') as file:
+
+        data=pickle.load(file)
+
+    return data
+
+lr= pickle.load(open('lr_model.pkl','rb'))
+
+
+st.title("House Price Predictor")
+st.write("""Please Select The Features of Your House""")
+area = st.text_input("Area:")
+bedrooms=st.text_input('Bedrooms:')
+bathrooms=st.text_input('Bathrooms:')
+stories=st.text_input('Stories:')
+parking=st.text_input('Parkings Available:')
+mainroad = st.selectbox("Main Road", ['yes', 'no'])
+airconditioning = st.selectbox("Air Conditioning?", ['yes', 'no'])
+furnishingstatus=st.selectbox("Furnishing Status",['furnished','semi-furnished','unfurnished'])
+ok=st.button("Predict")
+if ok:
+    # Prepare the input data based on user input
+    x = pd.DataFrame({
+        'area': [float(area)],
+        'bedrooms': [int(bedrooms)],
+        'bathrooms': [int(bathrooms)],
+        'stories': [int(stories)],
+        'mainroad': [mainroad],
+        'airconditioning': [airconditioning],
+        'parking': [int(parking)],
+        'furnishingstatus': [furnishingstatus]
+    })  
+    x['mainroad']=le.fit_transform(x['mainroad'])
+    x['airconditioning']=le.fit_transform(x['airconditioning'])
+    x['furnishingstatus']=le.fit_transform(x['furnishingstatus'])
+    x[['area']]=Scr.fit_transform(x[['area']])
+    pred=int(lr.predict(x))
+
+        
+    st.write(f"Estimated Cost of the House will be: {u'\u20B9'}{pred}")
