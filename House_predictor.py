78994206bbb7a264ed2b6fdf73dd129254b7b703
Abhinandan Saini <123210753+AbhinandanS4@users.noreply.github.com>
2024-10-28 02:02:04 +0530

    Update House_predictor.py

--- House_predictor.py
+++ House_predictor.py
@@ -25,7 +25,7 @@ lr= pickle.load(open('lr_model.pkl','rb'))
 
 st.title("House Price Predictor")
 st.write("""Please Select The Features of Your House""")
-area = st.slider("Area (in sq ft.):",5000,1000,17000)
+area = st.slider("Area (in sq ft.):",1000,17000,5000)
 bedrooms=st.text_input('Bedrooms:')
 bathrooms=st.text_input('Bathrooms:')
 stories=st.text_input('Stories:')
