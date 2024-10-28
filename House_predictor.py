93131268d97ca25d2cd39e1fb7090d72874577f6
Abhinandan Saini <123210753+AbhinandanS4@users.noreply.github.com>
2024-10-28 18:10:50 +0530

    Update House_predictor.py

--- House_predictor.py
+++ House_predictor.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
 import pickle
-
+import numpy as np
 url = 'https://raw.githubusercontent.com/AbhinandanS4/House_pred/refs/heads/main/Housing.csv'
 df = pd.read_csv(url, index_col=0)
 df.drop(columns=['guestroom','basement','hotwaterheating','prefarea'],inplace=True)
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=12)
+X_train['mainroad']=le.fit_transform(X_train['mainroad'])
+X_train['airconditioning']=le.fit_transform(X_train['airconditioning'])
+X_train['furnishingstatus']=le.fit_transform(X_train['furnishingstatus'])
 
 le=LabelEncoder()
 Scr=StandardScaler()
@@ -24,6 +30,7 @@ lr= pickle.load(open('lr_model.pkl','rb'))
 
 
 st.title("House Price Predictor")
+selection=st.selectbox("Select Your Prediction Type",['Ranged','Discrete'])
 st.write("""Please Select The Features of Your House""")
 area = st.slider("Area (in sq ft.):",100,17000,5000)
 bedrooms=st.slider('Bedrooms:',0,15,10)
@@ -54,13 +61,19 @@ if ok:
     x['airconditioning']=le.fit_transform(x['airconditioning'])
     x['furnishingstatus']=le.fit_transform(x['furnishingstatus'])
     x['prefarea']=le.fit_transform(x['prefarea'])
+    def lr_prediction_range(model, X_new, confidence_level=0.95):
+        predictions = model.predict(x)
+        prediction_std = np.std(model.predict(X_train) - y_train)
+        margin_of_error = prediction_std * 1.2  # 1.2 corresponds to a 20% confidence interval
+        lower_bound = predictions - margin_of_error
+        upper_bound = predictions + margin_of_error
+        return lower_bound, upper_bound
     pred=int(lr.predict(x))
-
-        
-    st.write(f"Estimated Cost of the House will be: {u'\u20B9'}{pred}")
-    st.header("Feedback")
-feedback = st.text_area("Please share your feedback about the prediction or the app's usability:")
-
+    lower_bound, upper_bound = lr_prediction_range(lr, x)
+    if selection=='Ranged':
+        st.write(f"Your House will cost will be in range: {u'\u20B9'}{round(lower_bound[0],2)} - {u'\u20B9'}{round(upper_bound[0],2)}" )
+    elif selection=='Discrete':
+        st.write(f"Estimated Cost of the House will be: {u'\u20B9'}{pred}")
 if st.button("Submit Feedback"):
     with open("feedback.txt", "a") as f:
         f.write(feedback + "\n")
